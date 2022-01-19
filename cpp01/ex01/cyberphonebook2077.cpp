9f17fa4f0a73a32e0022ee265aafa444f7fa669b
Pablo Diaz <pdiaz-pa@Pablos-iMac.local>
2022-01-19 06:50:18 -0800

    search implementado. falta punto

--- cpp01/ex01/cyberphonebook2077.cpp
+++ cpp01/ex01/cyberphonebook2077.cpp
@@ -6,12 +6,13 @@
 /*   By: pdiaz-pa <pdiaz-pa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 12:16:45 by pdiaz-pa          #+#    #+#             */
-/*   Updated: 2022/01/18 04:59:08 by pdiaz-pa         ###   ########.fr       */
+/*   Updated: 2022/01/19 06:49:23 by pdiaz-pa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include "Contact.class.hpp"
 #include "Phonebook.class.hpp"
 
@@ -44,6 +45,65 @@ int	add_mode(Contact *contact){
 	return(0);
 }
 
+void	print_head(){
+	std::cout << std::setfill(' ') << std::setw(10);
+	std::cout << "INDICE";
+	std::cout << "|";
+	std::cout << std::setfill(' ') << std::setw(10);
+	std::cout << "NOMBRE";
+	std::cout << "|";
+	std::cout << std::setfill(' ') << std::setw(10);
+	std::cout << "APELLIDO";
+	std::cout << "|";
+	std::cout << std::setfill(' ') << std::setw(10);
+	std::cout << "APODO";
+	std::cout << std::endl;
+}
+
+void	print_book( Phonebook *book ){
+	int i;
+	
+	i = 0;
+	while (i < 7){
+		std::cout << std::setfill(' ') << std::setw(10);
+		std::cout << i;
+		std::cout << "|";
+		std::cout << std::setfill(' ') << std::setw(10);
+		std::cout << book->array[i].getFirst();
+		std::cout << "|";
+		std::cout << std::setfill(' ') << std::setw(10);
+		std::cout << book->array[i].getLast();
+		std::cout << "|";
+		std::cout << std::setfill(' ') << std::setw(10);
+		std::cout << book->array[i].getNick();
+		std::cout << std::endl;		
+		i++;
+	}
+
+}
+
+int	search_mode ( Phonebook *book ){
+	
+	std::string	strindex;
+	int index;
+	print_head();
+	print_book(book);
+	std::cout << "Introduzca el índice de la agenda que quiere consultar" << std::endl;
+	std::getline (std::cin, strindex);
+
+	index = std::stoi(strindex);
+	if (index > 7 || index < 0)
+		std::cout << "El número debe estar comprendido entre 0 y 7" << std::endl;
+	else{
+		std::cout << "NOMBRE ->" << book->array[index].getFirst() << std::endl;
+		std::cout << "APELLIDO ->" << book->array[index].getLast() << std::endl;
+		std::cout << "APODO ->" << book->array[index].getNick() << std::endl;
+		std::cout << "NÚMERO ->" << book->array[index].getNumber() << std::endl;
+		std::cout << "SECRETO ->" << book->array[index].getSecret() << std::endl;
+	}
+	return (0);
+}
+
 int	main( void ){
     int again;
 	int i;
@@ -64,9 +124,12 @@ int	main( void ){
 			if (i == 7)
 				i = 7;
 		}
-
 		else if (mode.compare("SEARCH") == 0)
+		{
 			std::cout << "este es el modo -> " << mode << std::endl;
+			search_mode(&book);
+		}
+
 		else if (mode.compare("EXIT") == 0)
 			again = 0;
 		else
