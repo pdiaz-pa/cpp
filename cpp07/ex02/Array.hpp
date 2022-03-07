cd19f8e93922c137041e43967c9769e7c5eac625
Pablo Diaz pastor <pdiaz-pa@c2r17s3.42madrid.com>
2022-03-07 18:11:40 +0100

    07 02 asignacion y copia

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,3 +1,20 @@
+/*
+		Array<T>	&operator=(const Array<T> &array)
+		{
+			if (this != &array)
+			{
+				if (_size > 0)
+					delete [] _data;
+				_size = array._size;
+				_data = new T[array._size];
+				for (unsigned int i = 0; i < _size; i++)
+					_data[i] = array._data[i];
+			}
+			return (*this);
+		}
+
+*/
+
 #pragma once
 #include <iostream>
 template <class T>
@@ -7,20 +24,40 @@ private:
 	T *elements;
 	unsigned int _size;
 public:
-    T &operator[](unsigned int i) {
+	class outOfBounds : public std::exception
+	{
+	public:
+        virtual const char *what() const throw()
+        {
+        	return ("size selected is out of bounds");
+        }
+	};
 	
+    T &operator[](unsigned int i) {
        if( i > _size ) {
           std::cout << "Index out of bounds" << std::endl; 
           return elements[0];
        }
        return elements[i];
+	   try
+	   {
+		   if (i > _size){
+			   throw outOfBounds();
+		   }
+		   return elements[i];
+	   }
+	   catch(const std::exception& e)
+	   {
+		   std::cerr << e.what() << '\n';
+	   }
     }
+	
 	T &operator=(T const & rhs){
-	std::cout << "Assignement operator called" << std::endl;
-    if (this != &rhs)
-        this->elements = rhs.elements;
-
+		std::cout << "Assignement operator called" << std::endl;
+		this = &rhs;
+		return (*this);
 	}
+	
 	unsigned int size() const {
 		return (this->_size);
 	};
@@ -28,14 +65,15 @@ public:
 		std::cout << "Constructor with parameter called" << std::endl;
 	};
 	Array(Array & src){
-		*this = src;
+		*this = src; // Mal. Hay que hacer una copia del objeto con new, para que los cambios de un objeto no afecten a la copia
 		std::cout << "Copy constructor called" << std::endl;
-		return (this);
+		return;
 	};
 	Array() : _size(0){
 		std::cout << "Default constructor called" << std::endl;
 	};
 	~Array(){
+		std::cout << "Destructor called" << std::endl;
 		delete [] elements;
 	};
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -1,10 +1,51 @@
 #include "Array.hpp"
 
-int main ( void ){
-    Array <int>pepe(6);
+std::string getRandomWord(){
+    int random = (rand() % 40);
+    std::string word[40] = {"Década", "Interés", "Correcto", "Brillantemente", "Polilla", "Malicioso",
+                        "Colisión", "Cigarrillo", "Cerebro", "Dos", "Predicar", "Autoridad", "Simplista",
+                        "Silencioso", "Gloria", "Caliente", "Flotar", "Curro", "Mendicidad", "Ámbar",
+                        "Mezclada", "Divertido", "Soplar", "Descontaminación", "Despertar", "Vigoroso",
+                        "Piña", "Maravilloso", "Seco", "Diplomático", "Película", "Chiflado", "Traed",
+                        "Pestillo", "Hígado", "Arriesgado", "Rayo", "Comenzar", "Atrapar", "Biológico"};
     
-    std::cout << pepe[1] << std::endl;
+    return (word[random]);
+}
 
-    Array <std::string>cuerda(3);
-    std::cout << cuerda[2] << std::endl;
+int main ( void ){
+    //-----INTS-----
+    Array <int>nums(10);
+    srand(time(0));
+    for(unsigned int i = 0; i < nums.size(); i++){
+        int random = (rand() % 100);
+        nums[i] = random;
+    }
+    for(unsigned int i = 0; i < nums.size(); i++){
+            std::cout << nums[i] << std::endl;
+    }
+    //-----STRINGS-----
+    Array <std::string>cuerda(8);
+    for(unsigned int i = 0; i < cuerda.size(); i++){
+        cuerda[i] = getRandomWord();
+    }
+    for(unsigned int i = 0; i < cuerda.size(); i++){
+            std::cout << cuerda[i] << std::endl;
+    }
+    //-----FLOATS-----
+    Array <float>flotar(4);
+    for(unsigned int i = 0; i < flotar.size(); i++){
+        float random = (rand() % 100);
+        flotar[i] = random;
+    }
+    // Solucionar asignación y copia.
+    /*Array <float>copia = flotar;
+    flotar[1] = 69.69f;
+    std::cout << "flotar" << std::endl;
+    for(unsigned int i = 0; i < flotar.size(); i++){
+            std::cout << flotar[i] << std::endl;
+    }
+    std::cout << "copia" << std::endl;
+    for(unsigned int i = 0; i < copia.size(); i++){
+            std::cout << copia[i] << std::endl;
+    }*/
 }
