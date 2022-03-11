1dae3c4e17718877b389d6f967a4c4fd3578b9c9
Pablo Diaz <pdiaz-pa@Pablos-iMac.local>
2022-03-11 05:02:55 -0800

    08 done mirar lo de shortestspan

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -2,10 +2,19 @@
 #include <iostream>
 #include <stack>
 #include <deque>
-template <class T, typename Container = std::deque<T> >
-class MutantStack: public std::stack<T, Container>{
+template <class T>
+class MutantStack: public std::stack<T>{
     public:
-        
+		
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		iterator begin(){return this->c.begin();}
+		const_iterator begin() const{return this->c.begin();}
+		iterator end(){return this->c.end();}
+		const_iterator end() const{return this->c.end();}
+		
+		MutantStack & operator=(const MutantStack<T> & rhs);
         MutantStack();
         ~MutantStack();   
         MutantStack(MutantStack const & src);
@@ -13,17 +22,24 @@ class MutantStack: public std::stack<T, Container>{
     private:
 };
 
-template <class T, typename Container>
-MutantStack<T, Container> ::MutantStack(){
-    std::cout << "Default Constructor called." << std::endl;
-}
-template <class T, typename Container>
-MutantStack<T, Container>::~MutantStack(){
-    std::cout << "Destructor called." << std::endl;
+template<class T>
+MutantStack<T> & MutantStack<T>::operator=(const MutantStack & rhs){
+	if (*this != rhs){
+		this->c = rhs.c;
+	}
+	return (*this);
 }
 
-/*
-    Nuestra clase MutantStack hereda de:
-        -Stack
-        -deque(el cual contiene un iterador)
-*/
+template<class T>
+MutantStack<T>::MutantStack(MutantStack const & src){
+	*this = src;
+}
+
+template <class T>
+MutantStack<T> ::MutantStack(){
+    std::cout << "Default Constructor called." << std::endl;
+}
+template <class T>
+MutantStack<T>::~MutantStack(){
+    std::cout << "Destructor called." << std::endl;
+}
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,10 +1,21 @@
 #include "MutantStack.hpp"
+#include <list>
+
 
 int	main(void)
 {
+	std::cout << "-----MUTANT STACK TEST-----" << std::endl;
+	std::list<int> mylist;
 	MutantStack<int> mstack;
+
     mstack.push(5);
 
+	MutantStack<int> equal;
+	equal = mstack;
+	std::cout << "equal top-->" << equal.top() << std::endl;
+	std::cout << "mstack top-->" << mstack.top() << std::endl;
+	MutantStack<int>::iterator itequal = equal.begin();
+	itequal++;
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
@@ -15,7 +26,6 @@ int	main(void)
 	std::cout << mstack.size() << std::endl;
 	mstack.push(0);
 	std::cout << std::endl;
-	/*
 	MutantStack<int>::iterator it = mstack.begin();
 	++it;
 	MutantStack<int>::iterator ite = mstack.end();
@@ -33,7 +43,26 @@ int	main(void)
 		int tmp = s.top();
 		s.pop();
 		std::cout << tmp << std::endl;
-	}*/
+	}
+
+	std::cout << "----LIST TEST----" <<std::endl;
+	mylist.push_back(7);
+	mylist.push_back(67);
+	mylist.push_back(22);
+	mylist.push_back(43);
+
+	
+	std::list<int>::iterator it1 = mylist.begin();
+	++it;
+	std::list<int>::iterator ite1 = mylist.end();
+	--it;
+	
+	while (it1 != ite1)
+	{
+		std::cout << *it1 << std::endl;
+		++it1;
+	}
+
+
 	return (0);
-    
 }
