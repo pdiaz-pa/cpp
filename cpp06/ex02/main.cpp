d5ef325c18854ac6430204bdbad35ef721467f6f
Pablo Diaz <pdiaz-pa@Pablos-iMac.local>
2022-04-12 05:00:48 -0700

    06 ready

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,21 +6,21 @@
 Base *generate (){
 	srand(time(NULL));
 	int dice = std::rand () % 3;
-	std::cout << "object type ";
+	std::cout << "(object type ";
 	if (dice == 0){
-		std::cout << "A created." << std::endl;
+		std::cout << "A created.)" << std::endl;
 		return (new A);
 	}
 	else if (dice == 1){
-		std::cout << "B created." << std::endl;
+		std::cout << "B created.)" << std::endl;
 		return(new B);
 	}
 	else if (dice == 2){
-		std::cout << "C created." << std::endl;
+		std::cout << "C created.)" << std::endl;
 		return (new C);
 	}
 	else{
-		std::cout << "Anything created." << std::endl;
+		std::cout << "Anything created.)" << std::endl;
 	}
 	return 0;
 }
@@ -40,44 +40,44 @@ void identify(Base *p){
 void identify(Base &p){
 	try
 	{
-		//p = dynamic_cast<A &>(p);
-		if (dynamic_cast<A *>(&p) != NULL)
-			std::cout << "A ";
+		p = dynamic_cast<A &>(p);
+		std::cout << "A" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		//std::cout << "No es A" << std::endl;
+		std::cout << "(No es A)->" << e.what() << std::endl;
 	}
 	try
 	{
-		if (dynamic_cast<B *>(&p) != NULL)
-			std::cout << "B ";
-		//p = dynamic_cast<B &>(p);
+		p = dynamic_cast<B &>(p);
+		std::cout << "B" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		//std::cout << "No es B" << std::endl;
+		std::cout << "(No es B)->" << e.what() << std::endl;
 	}
 	try
 	{
-		if (dynamic_cast<C *>(&p) != NULL)
-			std::cout << "C ";
-		//p = dynamic_cast<C &>(p);
+		p = dynamic_cast<C &>(p);
+		std::cout << "C" << std::endl;
 	}
 	catch(const std::exception& e)
 	{
-		//std::cout << "No es C" << std::endl;
+		std::cout << "(No es C)->" << e.what() << std::endl;
 	}
 }
 
 int main (void){
 	Base *base = generate();
-	std::cout << "The generated class is ";
+	std::cout << "The generated class is " << std::endl;
 	identify(*base);
 	std::cout << "type." << std::endl << std::endl;
 
-	std::cout << "The generated class is ";
+	std::cout << "The generated class is " << std::endl;
 	identify(base);
 	std::cout << "type." << std::endl;
+
+	Base *erroneo = new Base;
+	identify(erroneo);
 
 }
